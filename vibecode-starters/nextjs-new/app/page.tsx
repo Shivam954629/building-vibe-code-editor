ddf473712597e1a115ca2d6f379fb30ac2531f90
Shivam954629 <shivampatna44@gmail.com>
2026-02-23 00:07:51 +0530

    normal changes in code

--- vibecode-starters/nextjs-new/app/page.tsx
+++ vibecode-starters/nextjs-new/app/page.tsx
@@ -1,8 +1,8 @@
+import "./page.css";
+
 export default function Home() {
   return (
-    <main
-      style={{ fontFamily: "sans-serif", padding: "2rem", textAlign: "center" }}
-    >
+    <main className="main-container">
       <h1>▲ Next.js App</h1>
       <p>
         Your app is running! Start editing <code>app/page.tsx</code>
--- vibecode-starters/react-ts/src/main.tsx
+++ vibecode-starters/react-ts/src/main.tsx
@@ -1,13 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./main.css";
 
 function App() {
   return (
-    <div
-      style={{ fontFamily: "sans-serif", padding: "2rem", textAlign: "center" }}
-    >
-      <h1 style={{ color: "#ff6b6b" }}>Succesfully app developed</h1>
-      <p style={{ color: "#555" }}>
+    <div className="app-container">
+      <h1 className="app-title">Succesfully app developed</h1>
+      <p className="app-description">
         Your app is running! Start editing <code>src/main.tsx</code>
       </p>
     </div>
